4d9784272ed936ca7de8222937777a32b864bd7b
Bavaharsan-Nagarajah-Sky <bavaharsan.nagarajah@gmail.com>
2025-02-02 23:58:31 +0000

    Added CORS middleware

--- src/config/middleware.ts
+++ src/config/middleware.ts
@@ -5,8 +5,26 @@ import morgan from 'morgan';
 import { rateLimiter } from '../middlewares/rateLimiter';
 
 export default (app: Application) => {
+
+    const corsOptions = {
+        origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+            if (process.env.NODE_ENV === 'development') {
+                callback(null, true);
+            } else {
+                if (origin === process.env.ALLOWED_ORIGIN) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    };
+
     app.use(helmet());
-    app.use(cors());
+    app.use(cors(corsOptions));
     app.use(morgan('dev'));
     app.use(rateLimiter);
     app.use(express.json());
